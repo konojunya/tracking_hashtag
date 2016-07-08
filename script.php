c5668c69a10efd257efb20d77474eb014df6224a
konojunya <junya03dance@gmail.com>
2016-07-09 01:29:44 +0900

    my first commit

---
+++ script.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once 'lib/TwistOAuth.php';
+require_once 'lib/TwistException.php';
+require_once 'settings.php';
+
+set_time_limit(0);
+
+while (ob_get_level()) {
+    ob_end_clean();
+}
+
+try {
+	$to = new TwistOAuth('yT577ApRtZw51q4NPMPPOQ', '3neq3XqN5fO3obqwZoajavGFCUrC42ZfbrLXy5sCv8');
+  $to = $to->renewWithAccessTokenX($screen_name, $password);
+} catch (TwistException $e) {
+	$error = $e->getMessage();
+  echo $error.PHP_EOL;
+}
+
+$list_id_str = $to->post("lists/create",array("name"=>"junjunTest"))->id_str;
+echo "list ".$list_name." を作りました。\nlisten ".$target_hashtag."\n";
+
+$to->streaming('statuses/filter', function ($status) use ($to,$list_id_str) {
+	if (isset($status)) {
+		try {
+			$to->post("lists/members/create",array("list_id" => $list_id_str,"screen_name" => $status->user->screen_name));
+			echo $status->user->name." - @".$status->user->screen_name." をlistに追加しました！\n";
+		}catch (TwistException $e){
+			$error = $e->getMessage();
+			echo $error.PHP_EOL;
+		}
+		flush();
+	}
+},array('track' => $target_hashtag));
---
+++ settings.php
@@ -0,0 +1,6 @@
+<?php
+
+$screen_name = ""; // your twitter id 						ex) konojunya
+$password = ""; // your password 									ex) a1o4rnwirj
+$list_name = ""; // list name 										ex) 岡山ナイトリスト
+$target_hashtag = "#"; // target hashtag 				ex) #岡山ナイト
